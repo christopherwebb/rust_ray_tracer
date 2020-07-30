56779916761c4d1039774a07276e864fb391547c
Christopher Webb <christopher.webb@gmail.com>
2020-07-30 22:04:03 +0100

    Reduce cylinder compiler warnings

--- examples/cylinders.rs
+++ examples/cylinders.rs
@@ -40,8 +40,8 @@ fn cylinders(aspect : f32) -> Scene {
                 centre: Point3f { x: 1.0, y: 0.0, z: -1.0},
                 radius: 0.5,
                 phi_max: 2.0 * f32::consts::PI,
-                zMin: -0.25,
-                zMax: 0.25,
+                z_min: -0.25,
+                z_max: 0.25,
                 material: Material::make_lambertian(
                     Colour { r: 0.1, g: 0.2, b: 0.5 },
                 )
@@ -50,8 +50,8 @@ fn cylinders(aspect : f32) -> Scene {
                 centre: Point3f { x: 0.0, y: 0.0, z: -1.0},
                 radius: 0.5,
                 phi_max: 2.0 * f32::consts::PI,
-                zMin: -1.0,
-                zMax: 1.0,
+                z_min: -1.0,
+                z_max: 1.0,
                 material: Material::make_metal(
                     Colour { r: 0.8, g: 0.6, b: 0.2 },
                     1.0,
@@ -61,16 +61,16 @@ fn cylinders(aspect : f32) -> Scene {
                 centre: Point3f { x: -1.0, y: 0.0, z: -1.0},
                 radius: 0.5,
                 phi_max: 2.0 * f32::consts::PI,
-                zMin: -0.5,
-                zMax: 0.5,
+                z_min: -0.5,
+                z_max: 0.5,
                 material: Material::make_dielectric(1.5)
             },
             Cylinder {
                 centre: Point3f { x: -1.0, y: 0.0, z: -1.0},
                 radius: -0.45,
                 phi_max: 2.0 * f32::consts::PI,
-                zMin: -0.45,
-                zMax: 0.45,
+                z_min: -0.45,
+                z_max: 0.45,
                 material: Material::make_dielectric(1.5)
             },
         ],
--- src/shapes/cylinder.rs
+++ src/shapes/cylinder.rs
@@ -21,8 +21,8 @@ pub struct Cylinder {
     pub centre : Point3f,
     pub radius: f32,
     pub phi_max: f32,
-    pub zMin: f32,
-    pub zMax: f32,
+    pub z_min: f32,
+    pub z_max: f32,
     pub material: Material,
 }
 
@@ -30,8 +30,8 @@ impl Hitable for Cylinder {
     fn hit(&self, ray : &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
         let centre_vec = Vector3f::from(&self.centre);
         let oc = ray.origin() - &centre_vec;
-        let oz_min = self.zMin - &self.centre.z;
-        let oz_max = self.zMax - &self.centre.z;
+        let oz_min = self.z_min - &self.centre.z;
+        let oz_max = self.z_max - &self.centre.z;
 
         let a : f32 = &ray.b.x * &ray.b.x + &ray.b.y * &ray.b.y;
         let b : f32 = 2.0 * (&oc.x * &ray.b.x + &oc.y * &ray.b.y);
@@ -65,7 +65,7 @@ impl Hitable for Cylinder {
             let mut hit = ray.point_at_parameter(t_hit);
             let mut orig_hit = hit - &centre_vec;
 
-            let mut hit_rad = (orig_hit.x * orig_hit.x + orig_hit.y * orig_hit.y).sqrt();
+            // let mut hit_rad = (orig_hit.x * orig_hit.x + orig_hit.y * orig_hit.y).sqrt();
             // orig_hit.e[0] *= self.radius / hit_rad;
             // orig_hit.e[1] *= self.radius / hit_rad;
 
@@ -92,7 +92,7 @@ impl Hitable for Cylinder {
                 hit = ray.point_at_parameter(t_hit);
                 orig_hit = hit - &centre_vec;
 
-                hit_rad = (orig_hit.x * orig_hit.x + orig_hit.y * orig_hit.y).sqrt();
+                // hit_rad = (orig_hit.x * orig_hit.x + orig_hit.y * orig_hit.y).sqrt();
                 // hit.e[0] *= self.radius / hit_rad;
                 // hit.e[1] *= self.radius / hit_rad;
 
@@ -102,11 +102,11 @@ impl Hitable for Cylinder {
                 // hit.e[0] = orig_hit.e[0] + self.centre.e[0];
                 // hit.e[1] = orig_hit.e[1] + self.centre.e[1];
 
-                phi = orig_hit.y.atan2(orig_hit.x);
+                // phi = orig_hit.y.atan2(orig_hit.x);
             
-                if phi < 0.0 {
-                    phi += 2.0 * f32::consts::PI;
-                }
+                // if phi < 0.0 {
+                //     phi += 2.0 * f32::consts::PI;
+                // }
 
                 // if (orig_hit.z() < oz_min || orig_hit.z() > oz_max || phi > self.phi_max) {
                 //     return false;
@@ -117,9 +117,9 @@ impl Hitable for Cylinder {
                 }
             }
 
-            let u = phi / self.phi_max;
-            // let v = (hit.z() - oz_min) / (oz_max - oz_min);
-            let v = (orig_hit.z - oz_min) / (oz_max - oz_min);
+            // let u = phi / self.phi_max;
+            // // let v = (hit.z() - oz_min) / (oz_max - oz_min);
+            // let v = (orig_hit.z - oz_min) / (oz_max - oz_min);
 
             let dpdu = Vector3f {
                 x: -self.phi_max * orig_hit.y,
@@ -128,24 +128,24 @@ impl Hitable for Cylinder {
             };
             let dpdv = Vector3f { x: 0.0, y: 0.0, z: oz_max - oz_min };
 
-            let d2Pduu = -self.phi_max * self.phi_max * Vector3f { x: orig_hit.x, y: orig_hit.y, z: 0.0 };
-            let d2Pduv = Vector3f { x: 0.0, y: 0.0, z: 0.0 };
-            let d2Pdvv = Vector3f { x: 0.0, y: 0.0, z: 0.0 };
+            // let d2Pduu = -self.phi_max * self.phi_max * Vector3f { x: orig_hit.x, y: orig_hit.y, z: 0.0 };
+            // let d2Pduv = Vector3f { x: 0.0, y: 0.0, z: 0.0 };
+            // let d2Pdvv = Vector3f { x: 0.0, y: 0.0, z: 0.0 };
 
             // Compute coefficients for fundamental forms
-            let E = dot_vv(&dpdu, &dpdu);
-            let F = dot_vv(&dpdu, &dpdv);
-            let G = dot_vv(&dpdv, &dpdv);
+            // let E = dot_vv(&dpdu, &dpdu);
+            // let F = dot_vv(&dpdu, &dpdv);
+            // let G = dot_vv(&dpdv, &dpdv);
             let N = &cross(&dpdu, &dpdv).unit_vector();
-            let e = dot_vv(&N, &d2Pduu);
-            let f = dot_vv(&N, &d2Pduv);
-            let g = dot_vv(&N, &d2Pdvv);
+            // let e = dot_vv(&N, &d2Pduu);
+            // let f = dot_vv(&N, &d2Pduv);
+            // let g = dot_vv(&N, &d2Pdvv);
 
-            let invEGF2 = 1.0 / (E * G - F * F);
-            let dndu = &((f * F - e * G) * invEGF2 * dpdu +
-                                                   (e * F - f * E) * invEGF2 * dpdv).unit_vector();
-            let dndv = &((g * F - f * G) * invEGF2 * dpdu +
-                                                   (f * F - g * E) * invEGF2 * dpdv).unit_vector();
+            // let invEGF2 = 1.0 / (E * G - F * F);
+            // let dndu = &((f * F - e * G) * invEGF2 * dpdu +
+            //                                        (e * F - f * E) * invEGF2 * dpdv).unit_vector();
+            // let dndv = &((g * F - f * G) * invEGF2 * dpdu +
+            //                                        (f * F - g * E) * invEGF2 * dpdv).unit_vector();
 
             rec.t = t_hit;
             rec.p = hit;
